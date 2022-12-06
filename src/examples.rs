c0d8437a029998cff087c90dfe1b87cc8f5b8836
TURKCELL\TCAARAS <ali.aras@turkcell.com.tr>
2022-12-06 16:20:46 +0300

    prog.

--- src/examples.rs
+++ src/examples.rs
@@ -1,2 +1,3 @@
 pub mod array_vector;
 pub mod guess_game;
+pub mod simple_web_service;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
-mod examples;
+//mod examples;
+
+// fn main() {
+    // println!("Hello, world!");
+    // examples::array_vector::array_vector();
+    // examples::guess_game::guess_game();
+    // examples::simple_web_service::get();
+//}
+
+//#![feature(proc_macro_hygiene, decl_macro)]
+
+use rocket::
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
 
 fn main() {
-    println!("Hello, world!");
-    examples::array_vector::array_vector();
-    examples::guess_game::guess_game();
+    rocket::ignite().mount("/", routes![index]).launch();
 }
